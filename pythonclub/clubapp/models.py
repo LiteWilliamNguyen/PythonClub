df341ae198141352098d6b57401b722c366528e5
khanhtrinhng <49286034+khanhtrinhng@users.noreply.github.com>
2019-05-27 19:01:48 -0700

    Resource View

--- pythonclub/clubapp/models.py
+++ pythonclub/clubapp/models.py
@@ -2,32 +2,36 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class PythonType(models.Model):
-    pythontypename = models.CharField(max_length=255)
-    pythondescription = models.TextField(null=True,blank=True)
+class Meetings(models.Model):
+    meetingtitle = models.CharField(max_length= 255)
+    meetingdate = models.DateField()
+    meetingtime = models.DateTimeField()
+    meetinglocation = models.CharField(max_length= 255)
+    meetingAgenda = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return self.pythontypename
+        return self.meetingtitle
     
     class Meta:
-        db_table = 'pythontype'
-        verbose_name_plural = 'pythontypes'
+        db_table = 'Meeting'
+        verbose_name_plural="Meetings"
+    
 
-class MeetingMinute(models.Model):
-    meetingid = models.ForeignKey(PythonType, on_delete = models.DO_NOTHING)
-    attendance = models.ManyToManyField(User)
-    minutetext = models.TextField()
+class MeetingMinutes(models.Model):
+    meeting_id = models.CharField(max_length= 255)
+    attendance = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    minutetext = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return self.meetingid
+        return self.meeting_id
 
     class Meta:
-        db_table = 'Meeting'
-        verbose_name_plural = 'Meetings'
+        db_table = 'Meeting Minute'
+    
 
 class Resource(models.Model):
     resourcename = models.CharField(max_length = 255)
-    resourcetype = models.ForeignKey(PythonType, on_delete= models.DO_NOTHING)
+    resourcetype = models.CharField(max_length = 255)
     resourceurl = models.URLField(null=True,blank=True)
     resourcedateenter = models.DateField()
     userid = models.ForeignKey(User, on_delete = models.DO_NOTHING)
@@ -38,7 +42,7 @@ class Resource(models.Model):
     
     class Meta:
         db_table = 'resoure'
-        verbose_name_plural = 'resources'
+ 
 
 class Event(models.Model):
     eventtitle = models.CharField(max_length = 255)
--- pythonclub/clubapp/views.py
+++ pythonclub/clubapp/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Meetings, MeetingMinutes, Resource, Event
+
+
 
 # Create your views here.
 def index (request):
     return render(request, 'clubapp/index.html')
+
+def getMeetings(request):
+    meetings_list=Meetings.objects.all()
+    context={'meetings_list' : types_list}
+    return render(request,'clubapp/types.html', context=context)
+
+def getResource(request):
+    resouce_list=Resource.objects.all()
+    return render(request, 'clubapp/resource.html',{'resource_list' : resource_list})
+
