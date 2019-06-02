1bf45ef6d5c3e0b856d07f583e00156c3d4460f5
khanhtrinhng <49286034+khanhtrinhng@users.noreply.github.com>
2019-06-01 22:47:29 -0700

    more test commit

--- pythonclub/clubapp/tests.py
+++ pythonclub/clubapp/tests.py
@@ -2,7 +2,8 @@ from django.test import TestCase
 from django.urls import reverse
 from .models import MeetingMinutes, Meetings, Resource, Event
 from django.contrib.auth.models import User
-
+from .forms import MeetingsForm, ResourceForm, EventForm
+import datetime
 # Create your tests here.
 class MeetingTest(TestCase):
     def test_string(self):
@@ -25,3 +26,25 @@ class EventTest(TestCase):
 
     def test_table(self):
         self.assertEqual(str(Event._meta.db_table), 'event')
+
+class Meeting_Form_Test(TestCase):
+    def setUp(self):
+        self.user=User.objects.create(username="user1", password="P@ssw0rd1")
+        self.meet=Meetings.objects.create(meetingtitle='title1',)
+
+
+class Resource_Form_Test(TestCase):
+    def setUp(self):
+        self.user=User.objects.create(username="user1", password="P@ssw0rd1")
+
+    def test_ResourceForm(self):
+        data={
+            'resourcename': 'name1',
+            'resourcetype': 'some type',
+            'resourceurl': 'some url',
+            'resourcedateenter': datetime.datetime.now(),
+            'user_id': self.user,
+            'resourcedescription' : 'some description'
+        }
+        form=ResourceForm(data=data)
+        self.assertTrue(form.is_valid)
